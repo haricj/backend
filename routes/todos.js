11bc62f7285ba000fcb2e6159bc18d198d235061
haricj <68504920+haricj@users.noreply.github.com>
2021-10-17 22:21:27 +0800

    commit

---
+++ routes/todos.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var todoController = require('../controllers/todos')
+
+/* GET users listing. */
+router.post('/:id/comment', todoController.addComment)
+router.get('/:id', todoController.getTodo);
+router.get('/', todoController.getAllTodos);
+router.post('/', todoController.createTodo);
+router.patch('/:id', todoController.updateTodo);
+router.delete('/:id', todoController.deleteTodo);
+
+module.exports = router;
